19517f6ceb0086f639a1a4e28ead51142ed6e7ab
sameeraa4ever <sameeraa4ever@gmail.com>
2021-11-30 11:23:59 -0800

    setup heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-fetch("http://localhost:3000/weather?address=sacramento").then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location);
-            console.log(data.weather);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form');
 const address = document.querySelector("#address");
 
@@ -20,7 +9,7 @@ weatherForm.addEventListener('submit',(e)=>{
     if(!address.value){
         console.log("Pls provide valid address")
     }else{
-        fetch("http://localhost:3000/weather?address="+address.value).then((response)=>{
+        fetch("/weather?address="+address.value).then((response)=>{
             response.json().then((data)=>{
                 if(data.error){
                     document.querySelector("#error").textContent = data.error;
